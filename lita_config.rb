a027855bb2c0f3452bd62a79e3fefc4e474ed56b
Daniel Hum <dan@oneuphero.com>
2015-09-17 11:16:07 +0800

    setup admins and log level

--- lita_config.rb
+++ lita_config.rb
@@ -8,12 +8,15 @@ Lita.configure do |config|
   # The severity of messages to log. Options are:
   # :debug, :info, :warn, :error, :fatal
   # Messages at the selected level and above will be logged.
-  config.robot.log_level = :info
+  config.robot.log_level = (ENV["LOG_LEVEL"] || :info).to_sym
 
   # An array of user IDs that are considered administrators. These users
   # the ability to add and remove other users from authorization groups.
   # What is considered a user ID will change depending on which adapter you use.
   # config.robot.admins = ["1", "2"]
+  if admins = ENV["ROBOT_ADMINS"]
+    config.robot.admins = admins.split(',')
+  end
 
   # The adapter you want to connect with. Make sure you've added the
   # appropriate gem to the Gemfile.
